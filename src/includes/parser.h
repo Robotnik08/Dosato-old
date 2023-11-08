35a6a113d7c56654052256eb0ede71f461ba7dc6
Robotnik08 <89689979+Robotnik08@users.noreply.github.com>
2023-11-08 01:01:44 +0100

    fixed array parser bug

--- src/includes/parser.h
+++ src/includes/parser.h
@@ -282,14 +282,17 @@ Node parse (const char* full_code, Token* tokens, const int start, const int end
                 int p_values[] = OPERATOR_PRECEDENCE;
                 int exit_loop = 0;
                 for (int o = 0; o < (end - start)/2 && !exit_loop; o++) { // o is the offset, discarding the first and last token when parentheses are present
+                    if (tokens[start + o].type == TOKEN_PARENTHESIS && tokens[start + o].carry & BRACKET_SQUARE && tokens[end - o].type == TOKEN_PARENTHESIS && tokens[end - o].carry & BRACKET_SQUARE) {
+                        break; // found an array expression, don't parse it as a binary expression
+                    }
                     for (int p = 15; p > 0 && !exit_loop; p--) { // looping through the precedence values, meaning that the highest precedence is checked first
                         for (int i = end - o; i >= start + o; i--) { // looping backwards through the tokens
                             if (tokens[i].type == TOKEN_PARENTHESIS && tokens[i].carry & (BRACKET_ROUND | BRACKET_SQUARE)) {
                                 i = getBlockReverse(tokens, i);
-                                if (tokens[i].carry & BRACKET_SQUARE || tokens[i-1].type != TOKEN_OPERATOR) exit_loop = 1; // exit the loops
+                                if (tokens[i-1].type == TOKEN_PARENTHESIS && tokens[i-1].carry & BRACKET_ROUND) exit_loop = 1; // exit the loops
                             }
                             if (tokens[i].type == TOKEN_OPERATOR && p_values[tokens[i].carry] == p) {
-                                if (!(tokens[i-1].type == TOKEN_IDENTIFIER || tokens[i-1].type == TOKEN_STRING || tokens[i-1].type == TOKEN_NUMBER || full_code[tokens[i-1].start] == ')')) {
+                                if (!(tokens[i-1].type == TOKEN_IDENTIFIER || tokens[i-1].type == TOKEN_STRING || tokens[i-1].type == TOKEN_NUMBER || (tokens[i-1].type == TOKEN_PARENTHESIS && tokens[i-1].carry & (BRACKET_ROUND | BRACKET_SQUARE)))) {
                                     exit_loop = 1; // exit the loops
                                     continue;
                                 }
